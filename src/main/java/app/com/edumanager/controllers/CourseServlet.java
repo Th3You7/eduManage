bfb5fca7fefd0b6cc52f5eb46d987680f951cfca
Th3you7 <elbasiriy@gmail.com>
2025-02-19 14:52:18 +0100

    course model, course dao

--- src/main/java/app/com/edumanager/controllers/CourseServlet.java
+++ src/main/java/app/com/edumanager/controllers/CourseServlet.java
@@ -1,6 +1,29 @@
 package app.com.edumanager.controllers;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebServlet("/course/*")
 public class CourseServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        String action = req.getPathInfo();
+        System.out.println(action);
+
+
+    }
+
 }
+
+
