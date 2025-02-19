ab28eee5ff62b5e05848220483d42fee3b3d4a78
 <erraoudymalak1997@gmail.com>
2025-02-19 15:51:35 +0100

    add student model & dao

---
+++ src/main/java/app/com/edumanager/models/Student.java
@@ -0,0 +1,53 @@
+package app.com.edumanager.models;
+
+public class Student {
+    int id ;
+    int birthdate;
+    String name;
+    String email;
+
+    public Student(int id, int birthdate, String name, String email) {
+        this.id = id;
+        this.birthdate = birthdate;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Student( int birthdate, String name, String email) {
+        this.birthdate = birthdate;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(int birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/app/com/edumanager/models/StudentModel.java
+++
@@ -1,4 +0,0 @@
-package app.com.edumanager.models;
-
-public class StudentModel {
-}
