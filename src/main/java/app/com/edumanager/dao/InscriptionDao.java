0256dea4ce52ee63741614e2b1a6cd02d41f4fc4
Asmoune56 <taakatrachid18@gmail.com>
2025-02-22 17:30:51 +0100

    inscription filter by course and student

--- src/main/java/app/com/edumanager/dao/InscriptionDao.java
+++ src/main/java/app/com/edumanager/dao/InscriptionDao.java
@@ -1,7 +1,10 @@
 package app.com.edumanager.dao;
 
+import app.com.edumanager.models.Course;
 import app.com.edumanager.models.Inscriptoin;
+import app.com.edumanager.models.Student;
 
+import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +21,21 @@ public class InscriptionDao {
     private static  final  String INSERT = "insert into inscription (studentID, courseID, inscDate) values(?,?,?)";
     private static  final  String DELETE = "delete from inscription where id = ?";
     private static  final  String SELECT_ALL = "select * from inscription";
-    private static  final  String SELECT_BY_ID = "select * from inscription where id = ?";
+    private static  final  String SELECT_BY_ID = "select inscription.* , student.*, student.name as student_name, course.*, course.name as course_name " +
+                                                "from inscription " +
+                                                "inner join student on ?= student.id " +
+                                                "inner join course on ? = course.id ";
     private static  final  String UPDATE = "update inscription set studentID = ?, courseID = ? where id = ?";
+
+    private  static  final  String SELECT_ALL_BY_COURSE =  "select inscription.* , student.*, student.name as student_name, course.*, course.name as course_name " +
+            "from inscription " +
+            "inner join student on inscription.studentID = student.id " +
+            "inner join course on inscription.courseID = course.id where course.id = ?";
+    private  static  final  String SELECT_ALL_BY_STUDENT =  "select inscription.* , student.*, student.name as student_name, course.*, course.name as course_name " +
+            "from inscription " +
+            "inner join student on inscription.studentID = student.id " +
+            "inner join course on inscription.courseID = course.id where student.id = ?";
+
 
 
 
@@ -69,7 +85,17 @@ public class InscriptionDao {
                 int studentID = Integer.parseInt(rs.getString("studentID"));
                 int inscDate = Integer.parseInt(rs.getString("inscDate"));
 
-                inscription = new Inscriptoin(courseID, studentID, inscDate);
+                String studentName = rs.getString("student_name");
+                String studentEmail = rs.getString("email");
+                int studentdt = Integer.parseInt(rs.getString("studentdt"));
+                String courseName = rs.getString("course_name");
+                String courseDescription = rs.getString("description");
+
+                Course course = new Course(courseName, courseDescription);
+                Student student = new Student(studentID,studentdt,  studentEmail,studentName);
+
+
+                inscription = new Inscriptoin( student,course, inscDate);
             }
         }catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage());
@@ -89,12 +115,22 @@ public class InscriptionDao {
                 int courseID = Integer.parseInt(rs.getString("courseID"));
                 int studentID = Integer.parseInt(rs.getString("studentID"));
                 int inscDate = Integer.parseInt(rs.getString("inscDate"));
+
+                String studentName = rs.getString("student_name");
+                String studentEmail = rs.getString("email");
+                int studentdt = Integer.parseInt(rs.getString("birth_date"));
+                String courseName = rs.getString("course_name");
+                String courseDescription = rs.getString("description");
                 // create student
+                Student student = new Student(studentID,studentdt,  studentEmail,studentName);
 
                 // create course
 
+                Course course = new Course(courseName,courseDescription);
+
                 // create inscription
-                Inscriptoin inscription = new Inscriptoin(courseID, studentID, inscDate);
+                Inscriptoin inscription = new Inscriptoin(student, course, inscDate);
+
                 inscriptions.add(inscription);
             }
 
@@ -134,5 +170,81 @@ public class InscriptionDao {
 
     }
 
+    public List<Inscriptoin> getAllInscriptionByCourse(int courseID){
+        List<Inscriptoin> inscriptions = new ArrayList<Inscriptoin>();
+        try {
+            Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_BY_COURSE);
+            preparedStatement.setInt(1, courseID);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+               // int courseID = Integer.parseInt(rs.getString("courseID"));
+                int studentID = Integer.parseInt(rs.getString("studentID"));
+                int inscDate = Integer.parseInt(rs.getString("inscDate"));
+
+                String studentName = rs.getString("student_name");
+                String studentEmail = rs.getString("email");
+                int studentdt = Integer.parseInt(rs.getString("birth_date"));
+                String courseName = rs.getString("course_name");
+                String courseDescription = rs.getString("description");
+                // create student
+                Student student = new Student(studentID,studentdt,  studentEmail,studentName);
+
+                // create course
+
+                Course course = new Course(courseName,courseDescription);
+
+                // create inscription
+                Inscriptoin inscription = new Inscriptoin(student, course, inscDate);
+
+                inscriptions.add(inscription);
+            }
+
+        }catch (SQLException e ){
+            System.err.println("SQLException: " + e.getMessage());
+        }
+
+        return inscriptions;
+
+
+    }
+    public List<Inscriptoin> getAllInscriptionBystudent(int studentId){
+        List<Inscriptoin> inscriptions = new ArrayList<Inscriptoin>();
+        try {
+            Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_BY_STUDENT);
+            preparedStatement.setInt(1, studentId);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                int courseID = Integer.parseInt(rs.getString("courseID"));
+                int studentID = Integer.parseInt(rs.getString("studentID"));
+                int inscDate = Integer.parseInt(rs.getString("inscDate"));
+
+                String studentName = rs.getString("student_name");
+                String studentEmail = rs.getString("email");
+                int studentdt = Integer.parseInt(rs.getString("birth_date"));
+                String courseName = rs.getString("course_name");
+                String courseDescription = rs.getString("description");
+                // create student
+                Student student = new Student(studentID,studentdt,  studentEmail,studentName);
+
+                // create course
+
+                Course course = new Course(courseName,courseDescription);
+
+                // create inscription
+                Inscriptoin inscription = new Inscriptoin(student, course, inscDate);
+
+                inscriptions.add(inscription);
+            }
+
+        }catch (SQLException e ){
+            System.err.println("SQLException: " + e.getMessage());
+        }
+
+        return inscriptions;
+
+
+    }
 
 }
